6c98aa8b980dced526eab1d8d4a31fc677388478
TailssTF <tailss5@mail.ru>
2024-04-08 16:21:42 +0700

    feat(Header): добавил импорт из хранилища

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+
 import pizzaLogoSvg from "../assets/img/pizza-logo.svg";
 import Search from "./Search";
+import { useStores } from "../Store-context";
 
-function Header() {
+const Header = observer(() => {
+  const {
+    CartStore: { totalPrice, items },
+  } = useStores();
   return (
     <div className="header">
       <div className="container">
@@ -18,7 +24,7 @@ function Header() {
         <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice} ₽</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -49,12 +55,12 @@ function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{items.length}</span>
           </Link>
         </div>
       </div>
     </div>
   );
-}
+});
 
 export default Header;
