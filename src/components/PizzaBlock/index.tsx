f70e644355e305cb5878e890207c9cac0bf225ed
TailssTF <tailss5@mail.ru>
2024-04-10 14:13:25 +0700

    fix: переход к пицце по картинке и названию

--- src/components/PizzaBlock/index.tsx
+++ src/components/PizzaBlock/index.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
-import { useStores } from "../../Store-context";
 
+import { useStores } from "../../Store-context";
 import { pizzaType } from "../../stores/CartStore";
 
 export interface IPizza {
@@ -14,73 +14,84 @@ export interface IPizza {
   types: number[];
 }
 
-const PizzaBlock: React.FC<IPizza> = observer((pizza) => {
-  const { id, title, price, imageUrl, sizes, types } = pizza;
-  const {
-    CartStore: { items, addItem },
-  } = useStores();
-  const [selectedType, setSelectedType] = useState<number>(0);
-  const [selectedSize, setSelectedSize] = useState<number>(0);
-  const cartItem = items.find((obj) => obj.id == id);
+const PizzaBlock: React.FC<IPizza> = observer(
+  ({ id, title, price, imageUrl, sizes, types }) => {
+    const {
+      CartStore: { items, addItem },
+    } = useStores();
+    const [selectedType, setSelectedType] = useState<number>(0);
+    const [selectedSize, setSelectedSize] = useState<number>(0);
+    const cartItem = items.find((obj) => obj.id == id);
 
-  const onAddPizza = () => {
-    addItem({ id, title, price, imageUrl, sizes, selectedSize, selectedType });
-  };
+    const onAddPizza = () => {
+      addItem({
+        id,
+        title,
+        price,
+        imageUrl,
+        sizes,
+        selectedSize,
+        selectedType,
+      });
+    };
 
-  return (
-    <div className="pizza-block__wrapper">
-      <div className="pizza-block">
-        <img className="pizza-block__image" src={imageUrl} alt={title} />
-        <h4 className="pizza-block__title">{title}</h4>
-        <div className="pizza-block__selector">
-          <ul>
-            {types.map((type) => (
-              <li
-                key={type}
-                className={selectedType == type ? "active" : ""}
-                onClick={() => setSelectedType(type)}
-              >
-                {pizzaType[type]}
-              </li>
-            ))}
-          </ul>
-          <ul>
-            {sizes.map((size, index) => (
-              <li
-                key={index}
-                className={selectedSize == index ? "active" : ""}
-                onClick={() => setSelectedSize(index)}
-              >
-                {size} см.
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="pizza-block__bottom">
-          <div className="pizza-block__price">от {String(price)} ₽</div>
-          <button
-            onClick={onAddPizza}
-            className="button button--outline button--add"
-          >
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+    return (
+      <div className="pizza-block__wrapper">
+        <div className="pizza-block">
+          <Link to={`/pizza/${id}`}>
+            <img className="pizza-block__image" src={imageUrl} alt={title} />
+            <h4 className="pizza-block__title">{title}</h4>
+          </Link>
+          <div className="pizza-block__selector">
+            <ul>
+              {types.map((type) => (
+                <li
+                  key={type}
+                  className={selectedType == type ? "active" : ""}
+                  onClick={() => setSelectedType(type)}
+                >
+                  {pizzaType[type]}
+                </li>
+              ))}
+            </ul>
+            <ul>
+              {sizes.map((size, index) => (
+                <li
+                  key={index}
+                  className={selectedSize == index ? "active" : ""}
+                  onClick={() => setSelectedSize(index)}
+                >
+                  {size} см.
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="pizza-block__bottom">
+            <div className="pizza-block__price">от {String(price)} ₽</div>
+            <button
+              onClick={onAddPizza}
+              className="button button--outline button--add"
             >
-              <path
-                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                fill="white"
-              />
-            </svg>
-            <span>Добавить</span>
-            {cartItem?.count && <i>{cartItem.count}</i>}
-          </button>
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                  fill="white"
+                />
+              </svg>
+              <span>Добавить</span>
+              {cartItem?.count && <i>{cartItem.count}</i>}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default PizzaBlock;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import { useStores } from "../Store-context";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import qs from "qs";
 
 import Categories from "../components/Categories";
@@ -33,11 +33,7 @@ const Home: React.FC = observer(() => {
   ));
   const pizzas =
     items.length > 0
-      ? items.map((pizza) => (
-          <Link to={`/pizza/${pizza.id}`} key={pizza.id}>
-            <PizzaBlock {...pizza} />
-          </Link>
-        ))
+      ? items.map((pizza) => <PizzaBlock {...pizza} key={pizza.id} />)
       : [];
 
   const url = new URL("https://660bdea73a0766e85dbcc139.mockapi.io/items");
