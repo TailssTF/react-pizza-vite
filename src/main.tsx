09be43de173550dd2c580b2dd7cddd77bb121134
TailssTF <tailss5@mail.ru>
2024-04-10 10:37:57 +0700

    fix(main): удалил лишние импорты

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
