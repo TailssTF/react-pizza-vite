9b82ac1763ba693aec744066120480e499039645
TailssTF <tailss5@mail.ru>
2024-04-10 17:18:56 +0700

    feat: добавил ленивую загрузку

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-import PizzaDetails from "./pages/PizzaDetails";
 
 import "./scss/app.scss";
 import MainLayout from "./layouts/MainLayout";
+
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
+const NotFound = lazy(
+  () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound")
+);
+const PizzaDetails = lazy(
+  () => import(/* webpackChunkName: "PizzaDetails" */ "./pages/PizzaDetails")
+);
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<PizzaDetails />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Идет загрузка...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Идет загрузка...</div>}>
+              <PizzaDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Идет загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
