129344cd5aa564374a5968dd66deb1c0ca83055a
TailssTF <tailss5@mail.ru>
2024-04-12 10:55:31 +0700

    fix: перенес storeContext

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,20 +1,16 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { StoreContext } from "../Store-context";
-import Store from "../stores/Store";
 import { Header } from "../components/";
 
 const MainLayout: React.FC = () => {
   return (
     <div className="wrapper">
-      <StoreContext.Provider value={new Store()}>
-        <Header />
-        <main className="content">
-          <Suspense fallback={<div>Идет загрузка...</div>}>
-            <Outlet />
-          </Suspense>
-        </main>
-      </StoreContext.Provider>
+      <Header />
+      <main className="content">
+        <Suspense fallback={<div>Идет загрузка...</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-
 import { BrowserRouter } from "react-router-dom";
 
+import App from "./App.tsx";
+import { StoreContext } from "./Store-context";
+import Store from "./stores/Store";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StoreContext.Provider value={new Store()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreContext.Provider>
 );
