ca554cbb6340b7f74b279cdb3262675e509ce31c
TailssTF <tailss5@mail.ru>
2024-04-15 17:02:31 +0700

    fix(Details): lдобавил кнопку назад

--- src/pages/PizzaDetails.tsx
+++ src/pages/PizzaDetails.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { IPizza } from "../components/PizzaBlock";
 
@@ -31,6 +31,25 @@ const PizzaDetails: React.FC = observer(() => {
     <div className="container">
       {pizza ? (
         <div className="content__pizza-details">
+          <Link to="/" className="button button--outline button--go-back">
+            <svg
+              width="8"
+              height="14"
+              viewBox="0 0 8 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 13L1 6.93015L6.86175 1"
+                stroke="#D3D3D3"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+
+            <span>Вернуться назад</span>
+          </Link>
           <img src={pizza.imageUrl} />
           <h2>{pizza.title}</h2>
           <h4>{pizza.price} ₽</h4>
